900e7c8d4a16c5d7e90ece96c4271e6e8788c148
Atthie Mulongo <atthie27@gmail.com>
2023-03-14 16:24:02 +0100

    Structure html de la page en mobile first

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div className='container'>
+              <div>
+                  <h1>MES AMIS ROBOTS</h1>
+              </div>
+              <div>
+                  <input type="text" name="" id="" placeholder='   Rechercher par nom' />
+              </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
