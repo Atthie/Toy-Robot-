c34faaf3ee6ea0412bd182ff6d738aaee2ba7931
Atthie Mulongo <atthie27@gmail.com>
2023-03-24 13:14:36 +0100

    Implementation de la fonction fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,52 @@
 import Robot from './Robot';
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
-  useEffect(()=>{},[])
+  const [robotData, setRobotData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => {
+        setRobotData(data);
+      });
+  }, []);
+
+  function filterRobots(robotData, searchTerm) {
+    return robotData.filter(
+      function(robot) {
+      return robot.name.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+  }
+  const filteredRobots = filterRobots(robotData, searchTerm);
+
+
+  function recherche(event) {
+    const value = event.target.value;
+    if (value.length >= 3) {
+      setSearchTerm(value);
+    } else {
+      setSearchTerm('');
+    }
+  }
   return (
     <div className="App">
-        <div className='container'>
-              <div className='titre'>
-                  <h1>MES AMIS ROBOTS</h1>
-              </div>
-              <div className='input'>
-                  <input type="text" name="" id="" placeholder='   Rechercher par nom' />
-              </div>
-              <div className='robot'>
-                <div className='row'>
-                    <Robot nom='Hervin howen ' proprio='lorem@mel' image='./images/W33.png'/>
-                    <Robot nom='Hervin howen ' proprio='lorem@mel' image='./images/W33.png'/>
-                    <Robot nom='Hervin howen ' proprio='lorem@mel' image='./images/9WO.png'/>
-                    <Robot nom='Hervin howen ' proprio='lorem@mel' image='./images/EQ6.png'/>
-                    <Robot nom='Hervin howen ' proprio='lorem@mel' image='./images/9WO.png'/>
-                    <Robot nom='Hervin howen ' proprio='lorem@mel' image='./images/EQ6.png'/>
-                    <Robot nom='Hervin howen ' proprio='lorem@mel' image='./images/08Q.png'/>
-                    <Robot nom='Hervin howen ' proprio='lorem@mel' image='./images/9WO.png'/>
-                    <Robot nom='Hervin howen ' proprio='lorem@mel' image='./images/08Q.png'/>
-                    <Robot nom='Hervin howen ' proprio='lorem@mel' image='./images/9WO.png'/>
-                  
-                </div>           
-              </div>
-
+      <div className='container'>
+        <div className='titre'>
+          <h1>MES AMIS ROBOTS</h1>
         </div>
+        <div className='input'>
+          <input type="text" name="" id="" placeholder='Rechercher par nom'  onChange={recherche} />
+        </div>
+        <div className='robot'>
+          <div className='row'>
+            {filteredRobots.map(robot => (
+              <Robot key={robot.id} nom={robot.name} mail={robot.email} image={robot.name}  />
+            ))}
+          </div>           
+        </div>
+      </div>
     </div>
   );
 }
--- src/Robot.jsx
+++ src/Robot.jsx
@@ -1,15 +1,13 @@
 import './robot.css'
 function Robot(props) {
-    
     return (
         <div className="containerRobot">
             <div className="image">
             <img src={'https://robohash.org/' + props.image} alt={props.description} />
             </div>
             <h4> {props.nom} </h4>
-            <p>{props.proprio} </p>
+            <p>{props.mail} </p>
         </div>
     )
 }
-
 export default Robot;
