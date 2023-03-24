53d2df11c62f8e9742dcc037e1a4561c96140cbb
Atthie Mulongo <atthie27@gmail.com>
2023-03-24 09:01:05 +0100

    Integration de l'API afficher les images des robots

--- src/Robot.jsx
+++ src/Robot.jsx
@@ -4,7 +4,7 @@ function Robot(props) {
     return (
         <div className="containerRobot">
             <div className="image">
-                <img src= {props.image} alt={props.description} />
+            <img src={'https://robohash.org/' + props.image} alt={props.description} />
             </div>
             <h4> {props.nom} </h4>
             <p>{props.proprio} </p>
